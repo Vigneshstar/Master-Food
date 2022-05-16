e8caa5d420bb9edae48f0728aa78720106d7db2c
Jithu Joshy Jy <jithujoshyjy@gmail.com>
2022-05-16 12:18:10 +0530

    Add Entity Relations

---
+++ Master Food/Models/Auth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Master_Food.Models
+{
+	public class Auth
+	{
+		public class Signup
+		{
+			
+		}
+
+		public class Login
+		{
+
+		}
+	}
+}
